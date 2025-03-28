9a09daf351440e6be16d9f060df08c9d6e7be5ef
Johnathan <johnathan.star42@gmail.com>
2025-03-27 22:49:16 -0300

    init lib

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@
 export { createElement, Fragment } from "./jsx-runtime";
 export { useState, useEffect } from "./state";
 export { matchRoute } from "./matchRoute";
-export { createRouter } from "./createRoute";
-export { createContext } from "./context-vanilla";
+export {  render , useNavigation } from "./createRoute";
+export { createContext  } from "./context-vanilla";
